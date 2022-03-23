41e26584c3ccef441568e50241f65287e633a968
SEC-MASUM <masum.jess42@gmail.com>
2022-03-23 16:30:02 +0600

    displayed local storage 'shopping-cart' in UI

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,19 +9,30 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
+    console.log("Product Loads before fetch");
     fetch("products.json")
       .then((response) => response.json())
       .then((data) => setProducts(data));
+    console.log("Product Loaded");
   }, []);
 
   useEffect(() => {
+    console.log("Local Storage First Line", products);
     const storedCart = getStoredCart();
-    console.log(storedCart);
+    const savedCart = [];
+    // console.log(storedCart);
     for (const id in storedCart) {
       const addedProduct = products.find((product) => product.id === id);
-      console.log(addedProduct);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+        console.log(addedProduct);
+      }
     }
-  }, []);
+    setCart(savedCart);
+    console.log("Local Storage Finished");
+  }, [products]);
 
   const handleAddToCart = (product) => {
     // console.log("clicked", product);
