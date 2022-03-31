24e1893eccde0d073ee118d61dd21e57ddc143e8
SEC-MASUM <masum.jess42@gmail.com>
2022-03-31 19:26:56 +0600

    added react router dom package, and added setup to use Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
+import Order from "./components/Order/Order";
+import About from "./components/About/About";
+import Inventory from "./components/Inventory/Inventory";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/" element={<Shop></Shop>} />
+        <Route path="/shop" element={<Shop></Shop>} />
+        <Route path="/order" element={<Order></Order>} />
+        <Route path="/inventory" element={<Inventory></Inventory>} />
+        <Route path="/about" element={<About></About>} />
+      </Routes>
     </div>
   );
 }
