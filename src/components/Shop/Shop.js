7aa14f5abd2ddf0f6bf2413803507f5772e59ade
SEC-MASUM <masum.jess42@gmail.com>
2022-04-25 00:48:35 +0600

    feat : get pageCount using api and create pagination button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,14 +9,26 @@ import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
   // console.log(products);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/productCount`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const count = data.count;
+        const pages = Math.ceil(count / 10);
+        setPageCount(pages);
+      });
+  }, []);
 
   useEffect(() => {
     const storedCart = getStoredCart();
     console.log(storedCart);
     const savedCart = [];
     for (const id in storedCart) {
-      const addedProduct = products.find((product) => product.id === id);
+      const addedProduct = products.find((product) => product._id === id);
 
       if (addedProduct) {
         const quantity = storedCart[id];
@@ -31,10 +43,12 @@ const Shop = () => {
     // console.log("clicked", product);
     //do not do this: cart.push(product)
     let newCart = [];
-    const isExists = cart.find((product) => product.id === selectedProduct.id);
+    const isExists = cart.find(
+      (product) => product._id === selectedProduct._id
+    );
     if (isExists) {
       const restProduct = cart.filter(
-        (product) => product.id !== selectedProduct.id
+        (product) => product._id !== selectedProduct._id
       );
       selectedProduct.quantity = selectedProduct.quantity + 1;
       newCart = [...restProduct, selectedProduct];
@@ -44,7 +58,7 @@ const Shop = () => {
     }
 
     setCart(newCart);
-    addToDb(selectedProduct.id);
+    addToDb(selectedProduct._id);
   };
 
   return (
@@ -52,11 +66,16 @@ const Shop = () => {
       <div className="product-container">
         {products.map((product) => (
           <Product
-            key={product.id}
+            key={product._id}
             product={product}
             handleAddToCart={handleAddToCart}
           ></Product>
         ))}
+        <div className="pagination">
+          {[...Array(pageCount).keys()].map((number) => (
+            <button>{number + 1}</button>
+          ))}
+        </div>
       </div>
       <div className="order-container">
         <Cart cart={cart}>
