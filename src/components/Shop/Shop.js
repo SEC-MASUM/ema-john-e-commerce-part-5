b550f43591a32dec71b6cdafe26e51365e40c3ef
SEC-MASUM <masum.jess42@gmail.com>
2022-04-25 16:07:55 +0600

    refactor : remove useEffect whis was used to cart , and use useCart instaed of useState

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
@@ -8,7 +9,7 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useCart();
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
@@ -16,11 +17,11 @@ const Shop = () => {
   // console.log(products);
 
   useEffect(() => {
-    const url = `http://localhost:5000/product?page=${page}&size${size}`;
+    const url = `http://localhost:5000/product?page=${page}&size=${size}`;
     axios.get(url).then((data) => {
       setProducts(data.data);
     });
-  }, []);
+  }, [page, size]);
 
   useEffect(() => {
     const url = `http://localhost:5000/productCount`;
@@ -33,21 +34,6 @@ const Shop = () => {
       });
   }, []);
 
-  useEffect(() => {
-    const storedCart = getStoredCart();
-    console.log(storedCart);
-    const savedCart = [];
-    for (const id in storedCart) {
-      const addedProduct = products.find((product) => product._id === id);
-
-      if (addedProduct) {
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        savedCart.push(addedProduct);
-      }
-    }
-    setCart(savedCart);
-  }, [products]);
 
   const handleAddToCart = (selectedProduct) => {
     // console.log("clicked", product);
