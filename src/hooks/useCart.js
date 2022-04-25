9ed3446f03e5a09464ab1ab0ecab0d3b61c05a93
SEC-MASUM <masum.jess42@gmail.com>
2022-04-25 16:02:40 +0600

    feat : send oeder product's key to server and load only that products data

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,22 +1,37 @@
 import { useEffect, useState } from "react";
 import { getStoredCart } from "../utilities/fakedb";
 
-const useCart = (products) => {
+const useCart = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const storedCartInDb = getStoredCart();
     const savedProductInCart = [];
-    for (const id in storedCartInDb) {
-      const addedProductInCart = products.find((product) => product._id === id);
-      if (addedProductInCart) {
-        const quantity = storedCartInDb[id];
-        addedProductInCart.quantity = quantity;
-        savedProductInCart.push(addedProductInCart);
-      }
-    }
-    setCart(savedProductInCart);
-  }, [products]);
+    const keys = Object.keys(storedCartInDb);
+    console.log(keys);
+    fetch("http://localhost:5000/productByKeys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(keys),
+    })
+      .then((res) => res.json())
+      .then((products) => {
+        console.log(products);
+        for (const id in storedCartInDb) {
+          const addedProductInCart = products.find(
+            (product) => product._id === id
+          );
+          if (addedProductInCart) {
+            const quantity = storedCartInDb[id];
+            addedProductInCart.quantity = quantity;
+            savedProductInCart.push(addedProductInCart);
+          }
+        }
+        setCart(savedProductInCart);
+      });
+  }, []);
 
   return [cart, setCart];
 };
